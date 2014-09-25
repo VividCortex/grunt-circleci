aaa071ae2d6fd32a50fdb494bdd154a0621ce942
Ismael Ambrosi <ismaambrosi@gmail.com>
2014-09-25 16:38:46 -0300

    Created grunt task to check for the build status

---
+++ src/tasks/circleci.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * Sleeps for N seconds
+ *
+ * @param {Number} seconds
+ */
+function sleep(seconds) {
+    var future = (new Date()).getTime() + (seconds * 1e3);
+
+    // Do nothing
+    while (future > (new Date()).getTime()) {
+        /* jshint noempty: false */
+    }
+}
+
+module.exports = function (grunt) {
+    var StatusChecker = require('../status');
+
+    function getConfig(key) {
+        return grunt.config.get('circleci.' + key);
+    }
+
+
+    grunt.registerTask('circleci', 'Grunt plugin to work with CircleCI API', function () {
+        this.requiresConfig('circleci.token', 'circleci.username', 'circleci.project', 'circleci.commit');
+
+        var done = this.async(), statusChecker, options;
+
+        options = this.options(StatusChecker.defaultOptions);
+
+        statusChecker = new StatusChecker(getConfig('token'), getConfig('username'), getConfig('project'), options);
+
+        statusChecker.checkCommit(getConfig('commit')).then(function () {
+            done(true);
+        }, function (result) {
+            grunt.log.error(result);
+
+            done(false);
+        });
+    });
+};
